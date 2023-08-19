4926ec2abc0fb9f5222328e8b6e35c1e4bd8707f
souta-1326 <souta.tahara@gmail.com>
2023-08-19 19:11:35 +0900

    make xorshift64.hpp

--- project/tableturf/src/xorshift64.hpp
+++ project/tableturf/src/xorshift64.hpp
@@ -1,8 +1,11 @@
 #pragma once
-#include<cstdint>
+#include <cstdint>
+#include <random>
 std::uint64_t xorshift64()
 {
-	static std::uint64_t x = 88172645463325252;
+	static std::random_device rng;
+	static std::mt19937_64 engine(rng());
+	static std::uint64_t x = engine();
 	x ^= x << 13;
 	x ^= x >> 7;
 	return x ^= x << 17;
