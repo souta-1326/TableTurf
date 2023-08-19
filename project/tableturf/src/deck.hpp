18a54b73b5e9eb7724d4580844f61725b8d13726
souta-1326 <souta.tahara@gmail.com>
2023-08-19 19:11:40 +0900

    make Deck

--- project/tableturf/src/deck.hpp
+++ project/tableturf/src/deck.hpp
@@ -5,12 +5,16 @@
 #include <algorithm>
 #include "xorshift64.hpp"
 class Deck{
+public:
   //デッキのカードの枚数
-  static constexpr int N_card_in_deck = 15;
+  static constexpr int N_CARD_IN_DECK = 15;
   //手札のカードの枚数
-  static constexpr int N_card_in_hand = 4;
+  static constexpr int N_CARD_IN_HAND = 4; 
+  //最大ターン数
+  static constexpr int TURN_MAX = 12;
+private:
   //デッキの各カードのID
-  int card_id_in_deck[N_card_in_deck];
+  int card_id_in_deck[N_CARD_IN_DECK];
   //現在のターン数
   int current_turn;
 public:
@@ -29,22 +33,22 @@ Deck::Deck(std::initializer_list<int> init){
 }
 void Deck::reset(){
   //カードをシャッフル
-  for(int i=0;i<N_card_in_deck-1;i++){
-    std::swap(card_id_in_deck[i],card_id_in_deck[i+xorshift64()%(N_card_in_deck-i)]);
+  for(int i=0;i<N_CARD_IN_DECK-1;i++){
+    std::swap(card_id_in_deck[i],card_id_in_deck[i+xorshift64()%(N_CARD_IN_DECK-i)]);
   }
   //ターン数をリセット
   current_turn = 1;
 }
 void Deck::choose_card_by_index(int index){
-  assert(0 <= index && index < N_card_in_hand);
+  assert(0 <= index && index < N_CARD_IN_HAND);
   //選んだ手札と(4+ターン数)枚目のカードを入れ替えて、入れ替えたカードを新しい手札とする
   //最終ターンでは手札を入れ替えない
-  if(current_turn < 12) std::swap(card_id_in_deck[index],card_id_in_deck[N_card_in_hand+current_turn-1]);
+  if(current_turn < 12) std::swap(card_id_in_deck[index],card_id_in_deck[N_CARD_IN_HAND+current_turn-1]);
   //ターン数に1を足す
   current_turn++;
 }
 void Deck::choose_card_by_card_id(int card_id){
-  int index = std::find(card_id_in_deck,card_id_in_deck+N_card_in_hand,card_id)-card_id_in_deck;
-  assert(index < N_card_in_hand);
+  int index = std::find(card_id_in_deck,card_id_in_deck+N_CARD_IN_HAND,card_id)-card_id_in_deck;
+  assert(index < N_CARD_IN_HAND);
   choose_card_by_index(index);
 }
