3db5b3e121d3fcd9639a81a8eaadecf3c2b547a5
souta-1326 <souta.tahara@gmail.com>
2023-08-19 19:29:57 +0900

    make AI_only_pass

--- project/tableturf/src/AI_only_pass.hpp
+++ project/tableturf/src/AI_only_pass.hpp
@@ -2,7 +2,12 @@
 #include <utility>
 #include "board.hpp"
 #include "choice.hpp"
+#include "deck.hpp"
+#include "xorshift64.hpp"
 //常に手札からランダムに選んでパスするAI
-template<class stage> choice AI_only_pass(const Board<stage> &board,const bool is_P1,const int deck[Board<stage>::N_card_in_deck]){
-  
+template<class stage> Choice AI_only_pass(const Board<stage> &board,const bool is_P1,const Deck &deck){
+  assert(board.current_turn == deck.current_turn);
+  //0~3の整数をランダムに選んでその添字のカードを選びパスする
+  int choose_card_index = xorshift64()%4;
+  return Choice{Deck[choose_card_index],-1,false};
 }
