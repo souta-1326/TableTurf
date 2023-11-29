b85a2390d311e277a6c821b09c583e2523fd0ffe
souta-1326 <souta.tahara@gmail.com>
2023-11-29 16:39:50 +0900

    add battle.hpp

---
+++ project/tableturf/src/battle.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "board.hpp"
+#include "choice.hpp"
+#include "deck.hpp"
+#include "AI.hpp"
+template<class stage> void Battle(AI<stage> AI_P1,AI<stage> AI_P2){
+  //deckのchoose_card_by_なんとかを忘れない
+  Board<stage> board;
+  for(int current_turn = 1;current_turn <= 12;current_turn++){
+    
+  }
+}
