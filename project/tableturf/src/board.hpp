46e0473433a4be40dbc1443913cacc846f73a380
souta-1326 <souta.tahara@gmail.com>
2024-01-05 21:35:09 +0900

    add Choice functions

--- project/tableturf/src/board.hpp
+++ project/tableturf/src/board.hpp
@@ -12,10 +12,10 @@ public:
   static constexpr int TURN_MAX = 12;
   //盤面の縦、横、マス数
   static constexpr int N_SQUARE = stage::N_SQUARE;
-  //現在のターン数
+  //現在のターン数(1からスタート)
   int current_turn;
-  //P1とP2が使ったカードのID
-  int used_cards_P1[TURN_MAX],used_cards_P2[TURN_MAX];
+  //P1とP2が使ったカードのID(1-indexed)
+  int used_cards_P1[TURN_MAX+1],used_cards_P2[TURN_MAX+1];
   //P1の現在のSPポイントとP2の現在のSPポイント
   int SP_point_P1,SP_point_P2;
   //P1が既に使ったSPポイントとP2が既に使ったSPポイント
@@ -43,7 +43,7 @@ public:
   bool is_valid_placement(const bool is_placement_P1,const int card_id,const int status_id,const bool is_SP_attack) const;
   //両方のプレイヤーのカードを置く
   void put_both_cards_without_validation(const int card_id_P1,const int card_direction_P1,const int card_pos_H_P1,const int card_pos_W_P1,const bool is_pass_P1,const bool is_SP_attack_P1,const int card_id_P2,const int card_direction_P2,const int card_pos_H_P2,const int card_pos_W_P2,const bool is_pass_P2,const bool is_SP_attack_P2);
-  void put_both_cards_without_validation(const Choice choice_P1,const Choice choice_P2);
+  void put_both_cards_without_validation(const Choice<stage> choice_P1,const Choice<stage> choice_P2);
   void put_both_cards_without_validation(const int card_id_P1,const int status_id_P1,const bool is_SP_attack_P1,const int card_id_P2,const int status_id_P2,const bool is_SP_attack_P2);
   //片方のプレイヤーのカードを置く(ビジュアライザ専用)
   void put_P1_card_without_validation(const int card_id_P1,const int card_direction_P1,const int card_pos_H_P1,const int card_pos_W_P1,const bool is_pass_P1,const bool is_SP_attack_P1);
@@ -86,20 +86,16 @@ template<class stage> bool Board<stage>::is_valid_placement(const bool is_placem
   (is_SP_attack ? square_SP_P2:square_P2))).any();
 }
 template<class stage> void Board<stage>::put_both_cards_without_validation(const int card_id_P1,const int card_direction_P1,const int card_pos_H_P1,const int card_pos_W_P1,const bool is_pass_P1,const bool is_SP_attack_P1,const int card_id_P2,const int card_direction_P2,const int card_pos_H_P2,const int card_pos_W_P2,const bool is_pass_P2,const bool is_SP_attack_P2){
-  int status_id_P1 = (is_pass_P1 ? -1:stage::card_direction_and_place_to_id[card_id_P1][card_direction_P1][card_pos_H_P1][card_pos_W_P1]);
-  int status_id_P2 = (is_pass_P2 ? -1:stage::card_direction_and_place_to_id[card_id_P2][card_direction_P2][card_pos_H_P2][card_pos_W_P2]);
-  put_both_cards_without_validation(
-  card_id_P1,status_id_P1,is_SP_attack_P1,
-  card_id_P2,status_id_P2,is_SP_attack_P2);
+  put_both_cards_without_validation({card_id_P1,card_direction_P1,card_pos_H_P1,card_pos_W_P1,is_pass_P1,is_SP_attack_P1},{card_id_P2,card_direction_P2,card_pos_H_P2,card_pos_W_P2,is_pass_P2,is_SP_attack_P2});
 }
-template<class stage> void Board<stage>::put_both_cards_without_validation(const Choice choice_P1,const Choice choice_P2){
+template<class stage> void Board<stage>::put_both_cards_without_validation(const Choice<stage> choice_P1,const Choice<stage> choice_P2){
   put_both_cards_without_validation(
     choice_P1.card_id,choice_P1.status_id,choice_P1.is_SP_attack,
     choice_P2.card_id,choice_P2.status_id,choice_P2.is_SP_attack
   );
 }
 template<class stage> void Board<stage>::put_both_cards_without_validation(const int card_id_P1,const int status_id_P1,const bool is_SP_attack_P1,const int card_id_P2,const int status_id_P2,const bool is_SP_attack_P2){
-  //status_id_P1,P2_status_idが-1の時はパス
+  //status_id_P1,status_id_P2が-1の時はパス
   const std::bitset<N_SQUARE> &card_covered_square_P1 = stage::card_covered_square[card_id_P1][status_id_P1];
   const std::bitset<N_SQUARE> &card_covered_square_normal_P1 = stage::card_covered_square_normal[card_id_P1][status_id_P1];
   const std::bitset<N_SQUARE> &card_covered_square_SP_P1 = stage::card_covered_square_SP[card_id_P1][status_id_P1];
@@ -107,14 +103,15 @@ template<class stage> void Board<stage>::put_both_cards_without_validation(const
   const std::bitset<N_SQUARE> &card_covered_square_normal_P2 = stage::card_covered_square_normal[card_id_P2][status_id_P2];
   const std::bitset<N_SQUARE> &card_covered_square_SP_P2 = stage::card_covered_square_SP[card_id_P2][status_id_P2];
   //SPアタックの時の前処理
+  //スペシャルアタックが突っ込まれる通常マスを空にする
   if(is_SP_attack_P1){
     SP_point_used_P1 += cards[card_id_P1].SP_COST;
-    //一度SPマスを取ったら取り返されることはないので、P2_squareだけ塗り返す
+    //square_P2_specialが取り返されることはないので、square_P2だけ空きマスで塗り返す
     square_P2 &= ~card_covered_square_P1;
   }
   if(is_SP_attack_P2){
     SP_point_used_P2 += cards[card_id_P2].SP_COST;
-    //一度SPマスを取ったら取り返されることはないので、P1_squareだけ塗り替えす
+    //square_P1_specialが取り返されることはないので、square_P1だけ空きマスで塗り返す
     square_P1 &= ~card_covered_square_P2;
   }
 
@@ -220,10 +217,17 @@ template<class stage> void Board<stage>::put_both_cards_without_validation(const
   SP_point_P1 = square_SP_burning_P1.count()+pass_time_P1-SP_point_used_P1;
   SP_point_P2 = square_SP_burning_P2.count()+pass_time_P2-SP_point_used_P2;
   //P1_used_cardsとP2_used_cardsを更新
-  used_cards_P1[current_turn-1] = card_id_P1;
-  used_cards_P2[current_turn-1] = card_id_P2;
+  used_cards_P1[current_turn] = card_id_P1;
+  used_cards_P2[current_turn] = card_id_P2;
   //ターン数を更新
   current_turn++;
+
+  //アサーション
+  assert((square_P1&square_SP_P1) == square_SP_P1);
+  assert((square_P2&square_SP_P2) == square_SP_P2);
+  assert(((square_P1|square_SP_P1)&(square_P2|square_SP_P2)).count() == 0);
+  assert((square_SP_burning_P1&square_SP_P1) == square_SP_burning_P1);
+  assert((square_SP_burning_P2&square_SP_P2) == square_SP_burning_P2);
 }
 template<class stage> void Board<stage>::put_P1_card_without_validation(const int card_id_P1,const int card_direction_P1,const int card_pos_H_P1,const int card_pos_W_P1,const bool is_pass_P1,const bool is_SP_attack_P1){
   int status_id_P1 = (is_pass_P1 ? -1:stage::card_direction_and_place_to_id[card_id_P1][card_direction_P1][card_pos_H_P1][card_pos_W_P1]);
--- project/tableturf/src/choice.hpp
+++ project/tableturf/src/choice.hpp
@@ -1,2 +1,34 @@
 #pragma once
-struct Choice{int card_id;int status_id;bool is_SP_attack;};
+#include "stage_database.hpp"
+#include "card_database.hpp"
+template<class stage> struct Choice{
+  int card_id;int status_id;bool is_SP_attack;
+  Choice(const int card_id,const int status_id,const bool is_SP_attack):card_id(card_id),status_id(status_id),is_SP_attack(is_SP_attack){}
+  Choice(const int card_id,const int card_direction,const int card_pos_H,const int card_pos_W,const bool is_pass,const bool is_SP_attack);
+  //相手視点のChoiceを生成する(非破壊的処理)
+  Choice<stage> swap_player();
+};
+
+template<class stage> Choice<stage>::Choice(const int card_id,const int card_direction,const int card_pos_H,const int card_pos_W,const bool is_pass,const bool is_SP_attack):
+card_id(card_id),is_SP_attack(is_SP_attack){
+  assert(!(is_pass && is_SP_attack));//パスなのにSPアタックをしていたらエラー
+  if(is_pass) status_id = -1;
+  else{
+    status_id = stage::card_direction_and_place_to_id[card_id][card_direction][card_pos_H][card_pos_W];
+    assert(status_id != -1);//不正な置き方を検知
+  }
+}
+
+template<class stage> Choice<stage> Choice<stage>::swap_player(){
+  //パス(status_id==-1)ならそのまま
+  if(status_id == -1) return *this;
+  //status_idから、1P視点の(カードの向き、左上の行、左上の列)を求める
+  auto [direction,pos_H,pos_W] = stage::card_status[card_id][status_id];
+  //directionを反転
+  direction = (direction+2)%4;
+  //pos_H,pos_Wの反転 右下から左上へのシフト
+  pos_H = (stage::h-pos_H-1)-((direction%2==0 ? cards[card_id].H:cards[card_id].W)-1);
+  pos_W = (stage::w-pos_W-1)-((direction%2==0 ? cards[card_id].W:cards[card_id].H)-1);
+  //status_idを求める
+  return {card_id,stage::card_direction_and_place_to_id[card_id][direction][pos_H][pos_W],is_SP_attack};
+}
