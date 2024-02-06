00f4fb09adfb35a93b9a58e8021cf532c0be568f
souta-1326 <souta.tahara@gmail.com>
2024-02-06 19:44:54 +0900

    排他制御を行う

--- project/tableturf/src/xorshift64.hpp
+++ project/tableturf/src/xorshift64.hpp
@@ -1,11 +1,16 @@
 #pragma once
 #include <cstdint>
 #include <random>
+#include <mutex>
 std::uint64_t xorshift64()
 {
 	static std::random_device rng;
 	static std::mt19937_64 engine(rng());
 	static std::uint64_t x = engine();
+	//排他制御
+	static std::mutex load_mutex;
+	std::lock_guard<std::mutex> guard(load_mutex);
+
 	x ^= x << 13;
 	x ^= x >> 7;
 	return x ^= x << 17;
