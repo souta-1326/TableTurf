c8812482fa3a70e9b129377c457ddec07c339d72
souta-1326 <souta.tahara@gmail.com>
2024-02-11 21:01:58 +0900

    ニューラルネットワーク構造の実装

---
+++ project/tableturf/src/network/common.py
@@ -0,0 +1,4 @@
+N_CARD = 232
+STAGE = "Main Street"
+H = 26
+W = 9
---
+++ project/tableturf/src/network/network.py
@@ -0,0 +1,111 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+from network.common import *
+import time
+
+class ResBlock(nn.Module):
+  def __init__(self,filters,use_bias):
+    super(ResBlock,self).__init__()
+    
+    self.conv1 = nn.Conv2d(filters,filters,kernel_size=3,padding=1,bias=use_bias)
+    self.bn1 = nn.BatchNorm2d(filters)
+    self.conv2 = nn.Conv2d(filters,filters,kernel_size=3,padding=1,bias=use_bias)
+    self.bn2 = nn.BatchNorm2d(filters)
+
+  def forward(self,x):
+    x_copied = x
+    x = F.relu(self.bn1(self.conv1(x)))
+    x = self.bn2(self.conv2(x))
+    x += x_copied
+    x = F.relu(x)
+    return x
+
+# 入力 (batch_size,N_card*2+39,stage::h,stage::w)
+# 1x2 通常マス
+# 1x2 SPマス
+# 1 壁・盤面外
+# 12 何ターン目か
+# 11x2 SPポイントがkポイント以上溜まっているか(1≤k≤11)
+# N_card x2 デッキに含まれる未使用カード(手札を含む)か
+
+# 出力 (batch_size,(N_CARD*(H*W*8+1),(1)))
+INPUT_C = N_CARD*2+39
+ACTION_SPACE_OF_EACH_CARD = H*W*8+1
+
+class AlphaZeroResNet(nn.Module):
+  def __init__(self,H:int,W:int,n_blocks=19,filters=256,use_bias=False):
+    super(AlphaZeroResNet,self).__init__()
+
+    self.conv1 = nn.Conv2d(N_CARD*2+39,filters,kernel_size=3,padding=1,bias=use_bias)
+    self.bn1 = nn.BatchNorm2d(filters)
+
+    #: residual tower
+    self.blocks = nn.Sequential(*[ResBlock(filters,use_bias) for _ in range(n_blocks)])
+
+    #: policy (action) head
+    self.conv_p_action = nn.Conv2d(filters,4,kernel_size=1,bias=use_bias)
+    self.bn_p_action = nn.BatchNorm2d(4)
+    self.flat_p_action = nn.Flatten()
+    self.logits_action = nn.Linear(4*H*W,N_CARD*ACTION_SPACE_OF_EACH_CARD)
+
+    #: policy (redraw) head
+    self.conv_p_redraw = nn.Conv2d(filters,4,kernel_size=1,bias=use_bias)
+    self.bn_p_redraw = nn.BatchNorm2d(4)
+    self.flat_p_redraw = nn.Flatten()
+    self.logits_redraw = nn.Linear(4*H*W,2)
+
+    #: value head
+    self.conv_v = nn.Conv2d(filters,4,kernel_size=1,bias=use_bias)
+    self.bn_v = nn.BatchNorm2d(4)
+    self.flat_v = nn.Flatten()
+    self.fc1_v = nn.Linear(4*H*W,256)
+    self.fc2_v = nn.Linear(256,1)
+
+  def forward(self,x):
+    x = F.relu(self.bn1(self.conv1(x)))
+    
+    x = self.blocks(x)
+
+    #: policy (action) head
+    x_p_action = F.relu(self.bn_p_action(self.conv_p_action(x)))
+    x_p_action = self.flat_p_action(x_p_action)
+    logits_action = self.logits_action(x_p_action)
+
+    #: policy (redraw) head
+    x_p_redraw = F.relu(self.bn_p_redraw(self.conv_p_redraw(x)))
+    x_p_redraw = self.flat_p_redraw(x_p_redraw)
+    logits_redraw = self.logits_redraw(x_p_redraw)
+
+    #: value head
+    x_v = F.relu(self.bn_v(self.conv_v(x)))
+    x_v = self.flat_v(x_v)
+    x_v = self.fc2_v(self.fc1_v(x_v))
+    value = torch.tanh(x_v)
+
+    return logits_action,logits_redraw,value
+
+class AlphaZeroResNet_CPP(nn.Module):
+  def __init__(self,model:nn.Module):
+    super(AlphaZeroResNet_CPP,self).__init__()
+    self.model = model
+  def forward(self,x):
+    return torch.cat(self.model.forward(x),dim=1)
+
+def main():
+  H,W=26,9
+  model = AlphaZeroResNet(H,W,n_blocks=19).to("mps")
+  print(sum(p.numel() for p in model.parameters() if p.requires_grad))
+  dummy_input = torch.rand(64,N_CARD*2+39,H,W).to("mps")
+  torch.mps.synchronize()
+  start = time.time()
+  logits_action,logits_redraw,value = model(dummy_input)
+  torch.mps.synchronize()
+  elapsed_time = time.time() - start
+  print(elapsed_time)
+
+
+if __name__ == "__main__":
+  main()
