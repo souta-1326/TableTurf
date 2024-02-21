c00b5d91207c3a97be30081cef5eb3c205ba9f24
souta-1326 <souta.tahara@gmail.com>
2024-02-21 22:52:49 +0900

    高速化、時間計測の修正

--- project/tableturf/src/starter_deck_learning.py
+++ project/tableturf/src/starter_deck_learning.py
@@ -30,7 +30,7 @@ def train_mps(learning_rate,do_save_model:bool):
   # create dataloader
   with open(dataset_path,"rb") as file:
     train_data = pickle.load(file)
-  dataloader = DataLoader(train_data,batch_size=learning_batch_size,shuffle=True)
+  dataloader = DataLoader(train_data,batch_size=learning_batch_size,shuffle=True,pin_memory=True,num_workers=2)
 
   for _ in range(n_epochs):
     with tqdm(enumerate(dataloader),total=len(dataloader)) as pbar_loss:
@@ -82,7 +82,7 @@ def train_DDP(rank,learning_rate,do_save_model:bool):
   with open(dataset_path,"rb") as file:
     train_data = pickle.load(file)
   sampler = DistributedSampler(dataset=train_data,num_replicas=num_gpus,rank=rank,shuffle=True)
-  dataloader = DataLoader(train_data,batch_size=learning_batch_size,sampler=sampler)
+  dataloader = DataLoader(train_data,batch_size=learning_batch_size,sampler=sampler,num_workers=2,pin_memory=True)
 
   scaler = torch.cuda.amp.GradScaler()
   for epoch_id in range(n_epochs):
@@ -138,8 +138,8 @@ def main():
     # testplay & selfplay
     command = f"{starter_deck_selfplay_program} {num_cpus} {num_gpus} {device} {num_games_in_parallel} {num_games_in_selfplay} {num_games_in_testplay} {buffer_size} {PV_ISMCTS_num_simulations} {simple_ISMCTS_num_simulations} {diff_bonus} {dirichlet_alpha} {eps} {model_cpp_path} {data_path} {log_path}"
     print(command)
-    ret = subprocess.run(command,shell=True)
-    assert ret.returncode == 0
+    # ret = subprocess.run(command,shell=True)
+    # assert ret.returncode == 0
 
     print("selfplay done")
 
@@ -158,8 +158,10 @@ def main():
     start_learn_time = time.time()
     if device=="cuda":
       mp.spawn(train_DDP,args=(learning_rate,do_save_model),nprocs=num_gpus,join=True)
+      torch.cuda.synchronize()
     else:
       train_mps(learning_rate,do_save_model)
+      torch.mps.synchronize()
     end_learn_time = time.time()
 
     step_count += n_epochs*math.ceil(len(train_data))
