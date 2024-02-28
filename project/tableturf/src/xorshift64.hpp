d343306c291181dd98cf52408d57a6c90ea0a21a
souta-1326 <souta.tahara@gmail.com>
2024-02-28 23:35:53 +0900

    高速化のために排他制御を消す

--- project/tableturf/src/xorshift64.hpp
+++ project/tableturf/src/xorshift64.hpp
@@ -8,8 +8,8 @@ std::uint64_t xorshift64()
 	static std::mt19937_64 engine(rng());
 	static std::uint64_t x = engine();
 	//排他制御
-	static std::mutex load_mutex;
-	std::lock_guard<std::mutex> guard(load_mutex);
+	// static std::mutex load_mutex;
+	// std::lock_guard<std::mutex> guard(load_mutex);
 
 	x ^= x << 13;
 	x ^= x >> 7;
