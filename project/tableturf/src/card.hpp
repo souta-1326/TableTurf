6d5cf1cdfdc4a6dd4a9f9a5f8ef7b8998dd24d6c
souta-1326 <souta.tahara@gmail.com>
2025-06-22 15:16:24 +0900

    カードのメンバ変数にカード名を追加

--- project/tableturf/src/card.hpp
+++ project/tableturf/src/card.hpp
@@ -1,20 +1,21 @@
 #pragma once
+#include <string>
 class Card{
 public:
   static constexpr int MAX_H = 8;
   static constexpr int MAX_W = 8;
+  const std::u16string NAME;
   const int H,W,N_SQUARE,SP_COST;
   //時計回りにN度回転した時のマス (左上から詰めて)
   bool R0[MAX_H][MAX_W]={},R90[MAX_W][MAX_H]={},R180[MAX_H][MAX_W]={},R270[MAX_W][MAX_H]={};
   //スペシャルマスの位置(ない場合は共に-1)
   int R0_SP_H,R0_SP_W,R90_SP_H,R90_SP_W,R180_SP_H,R180_SP_W,R270_SP_H,R270_SP_W;
-  constexpr Card(const int h,const int w,const int n_square,const short r0[MAX_H][MAX_W],const int sp_cost);
+  constexpr Card(const std::u16string name,const int h,const int w,const int n_square,const short r0[MAX_H][MAX_W],const int sp_cost);
   //特別なカード(SP_COST==3,N_SQUARE==13,SPマスなし)
   constexpr bool is_special_card() const {return R0_SP_H == -1;};
 };
-constexpr Card::Card(const int h,const int w,const int n_square,const short r0[MAX_H][MAX_W],const int sp_cost):H(h),W(w),N_SQUARE(n_square),SP_COST(sp_cost),
+constexpr Card::Card(const std::u16string name,const int h,const int w,const int n_square,const short r0[MAX_H][MAX_W],const int sp_cost):NAME(name),H(h),W(w),N_SQUARE(n_square),SP_COST(sp_cost),
 R0_SP_H(-1),R0_SP_W(-1),R90_SP_H(-1),R90_SP_W(-1),R180_SP_H(-1),R180_SP_W(-1),R270_SP_H(-1),R270_SP_W(-1){
-  R0_SP_H = R0_SP_W = R90_SP_H = R90_SP_W = R180_SP_H = R180_SP_W = R270_SP_H = R270_SP_W = -1;
   for(int i=0;i<h;i++){
     for(int j=0;j<w;j++){
       R0[i][j] = r0[i][j];
--- project/tableturf/src/card_download.py
+++ project/tableturf/src/card_download.py
@@ -7,6 +7,8 @@ MAX_W = 8
 URL = "https://raw.githubusercontent.com/Leanny/splat3/master/data/mush/800/MiniGameCardInfo.json"
 text = requests.get(URL).text
 cards_data = json.loads(text)
+with open("card_name.txt",mode='r') as f:
+  card_names = f.read().split("\n")
 def data_format(data:dict):
   #Empty:0 Fill:1 Special:2
   card_format = [[0]*MAX_W for _ in range(MAX_H)]
@@ -28,9 +30,9 @@ def data_format(data:dict):
   for i in range(MAX_H):
     for j in range(MAX_W):
       card_format[i][j] = card_format[i+up_h][j+left_w] if (i+up_h < MAX_H and j+left_w < MAX_W) else 0
-  return data['Number'],h,w,data['Square'].count('Fill')+data['Square'].count('Special'),card_format,data['SpecialCost']
+  return data['Number'],card_names[data['Number']],h,w,data['Square'].count('Fill')+data['Square'].count('Special'),card_format,data['SpecialCost']
 #先頭にダミーを加える(0-indexed to 1-indexed)
-cards_data_format = [(0,0,0,0,[[0]*8 for _ in range(8)],0)]+[data_format(card_data) for card_data in cards_data]
+cards_data_format = [(0,"",0,0,0,[[0]*8 for _ in range(8)],0)]+[data_format(card_data) for card_data in cards_data]
 cards_data_format.sort()
 with open("card_database.hpp",mode='w') as f:
   print('#pragma once',file=f)
@@ -39,12 +41,12 @@ with open("card_database.hpp",mode='w') as f:
   print(f'constexpr int N_card = {N_card};',file=f)
   print('constexpr short cards_cell[N_card+1][Card::MAX_H][Card::MAX_W] = {',file=f)
   #リストの[]を{}に変更し、空白を無くす
-  for id,h,w,N_square,card_cell,SP_cost in cards_data_format:
+  for id,card_name,h,w,N_square,card_cell,SP_cost in cards_data_format:
     print(str(card_cell).replace('[','{').replace(']','}').replace(' ','')+",",file=f)
   print('};',file=f)
-  print('constexpr Card cards[N_card+1] = {',file=f)
-  for i,(id,h,w,N_square,card_cell,SP_cost) in enumerate(cards_data_format):
-    print(f'{"{"}{h},{w},{N_square},cards_cell[{i}],{SP_cost}{"}"},',file=f)
+  print('const Card cards[N_card+1] = {',file=f)
+  for i,(id,card_name,h,w,N_square,card_cell,SP_cost) in enumerate(cards_data_format):
+    print(f'{"{"}u"{card_name}",{h},{w},{N_square},cards_cell[{i}],{SP_cost}{"}"},',file=f)
   print('};',file=f)
 
 with open("network/common.py",mode='w') as f:
