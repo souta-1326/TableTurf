a9c1597dc652ce50c9ff3d36f881dc2915e3a637
souta-1326 <souta.tahara@gmail.com>
2025-08-08 15:19:10 +0900

    コンパイルエラーの原因となったconstexprを外した

--- project/tableturf/src/card.hpp
+++ project/tableturf/src/card.hpp
@@ -10,11 +10,11 @@ public:
   bool R0[MAX_H][MAX_W]={},R90[MAX_W][MAX_H]={},R180[MAX_H][MAX_W]={},R270[MAX_W][MAX_H]={};
   //スペシャルマスの位置(ない場合は共に-1)
   int R0_SP_H,R0_SP_W,R90_SP_H,R90_SP_W,R180_SP_H,R180_SP_W,R270_SP_H,R270_SP_W;
-  constexpr Card(const std::u16string name,const int h,const int w,const int n_square,const short r0[MAX_H][MAX_W],const int sp_cost);
+  Card(const std::u16string name,const int h,const int w,const int n_square,const short r0[MAX_H][MAX_W],const int sp_cost);
   //特別なカード(SP_COST==3,N_SQUARE==13,SPマスなし)
   constexpr bool is_special_card() const {return R0_SP_H == -1;};
 };
-constexpr Card::Card(const std::u16string name,const int h,const int w,const int n_square,const short r0[MAX_H][MAX_W],const int sp_cost):NAME(name),H(h),W(w),N_SQUARE(n_square),SP_COST(sp_cost),
+Card::Card(const std::u16string name,const int h,const int w,const int n_square,const short r0[MAX_H][MAX_W],const int sp_cost):NAME(name),H(h),W(w),N_SQUARE(n_square),SP_COST(sp_cost),
 R0_SP_H(-1),R0_SP_W(-1),R90_SP_H(-1),R90_SP_W(-1),R180_SP_H(-1),R180_SP_W(-1),R270_SP_H(-1),R270_SP_W(-1){
   for(int i=0;i<h;i++){
     for(int j=0;j<w;j++){
